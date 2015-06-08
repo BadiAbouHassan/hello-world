1a9b90cfd11d5ef9c856e4af5ca194ad55898e00
princebayan <prince_bayan@hotmail.com>
2015-06-09 01:00:47 +0300

    implement the view Role

--- template/template/Admin/viewRole.aspx.cs
+++ template/template/Admin/viewRole.aspx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using template.Controllers;
+using template.DBModel;
 
 namespace template.Admin.User
 {
@@ -10,7 +12,42 @@ namespace template.Admin.User
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                errMsgDiv.Style.Add("display", "none");
+                successMsgDiv.Style.Add("display", "none");
 
+                RoleController controller = new RoleController();
+
+                List<Role> roles = controller.getRole();
+
+                if (roles != null)
+                {
+                    for (int i = 0; i < roles.Count; i++)
+                    {
+                        TableRow tRow = new TableRow();
+                        Table1.Rows.Add(tRow);
+
+                        // Create a new cell and add it to the row.
+                        TableCell nameCell = new TableCell();
+                        nameCell.Text = roles[i].roleID.ToString();
+                        tRow.Cells.Add(nameCell);
+
+                        TableCell descCell = new TableCell();
+                        descCell.Text = roles[i].roleName;
+                        tRow.Cells.Add(descCell);
+
+                        TableCell durationCell = new TableCell();
+                        durationCell.Text = roles[i].predefined == 0 ? "False": "True";
+                        tRow.Cells.Add(durationCell);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errMsgDiv.Style.Remove("display");
+                errMsg.Text = ex.Message;
+            }
         }
     }
 }
