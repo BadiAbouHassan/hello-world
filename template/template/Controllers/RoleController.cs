6c26b52d6439f227b75262a35bf78e242bf17ee2
princebayan <prince_bayan@hotmail.com>
2015-06-09 00:55:58 +0300

    implement role controller

---
+++ template/template/Controllers/RoleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using template.DBModel;
+using template.DBService;
+
+namespace template.Controllers
+{
+    public class RoleController
+    {
+        private RoleService roleService;
+        public RoleController()
+        {
+            this.roleService = new RoleService();
+        }
+
+        public Boolean addRole(Role role)
+        {
+            return this.roleService.add(role);
+        }
+
+        public List<Role> getRole(int roleID= 0)
+        {
+            return this.roleService.getRoles(roleID);
+        }
+    }
+}
