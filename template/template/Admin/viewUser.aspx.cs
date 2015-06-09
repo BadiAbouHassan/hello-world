7ba71bb1b017c92ff481737536ee0d41b3e1ed49
princebayan <prince_bayan@hotmail.com>
2015-06-09 23:40:14 +0300

    changes in view user page

--- template/template/Admin/viewUser.aspx.cs
+++ template/template/Admin/viewUser.aspx.cs
@@ -3,14 +3,78 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using template.Controllers;
+using template.DBModel;
 namespace template.Admin.User
 {
     public partial class viewUser : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                errMsgDiv.Style.Add("display", "none");
+                successMsgDiv.Style.Add("display", "none");
 
+                UserController controller = new UserController();
+
+                List<DBModel.User> users = controller.getUsers();
+
+                if (users != null)
+                {
+                    for (int i = 0; i < users.Count; i++)
+                    {
+                        TableRow tRow = new TableRow();
+                        Table1.Rows.Add(tRow);
+
+                        // Create a new cell and add it to the row.
+                        TableCell nameCell = new TableCell();
+                        nameCell.Text = users[i].userID.ToString(); 
+                        tRow.Cells.Add(nameCell);
+
+                        TableCell descCell = new TableCell();
+                        descCell.Text = users[i].username;
+                        tRow.Cells.Add(descCell);
+
+                        TableCell password = new TableCell();
+                        password.Text = users[i].password;
+                        tRow.Cells.Add(password);
+
+                        TableCell firstname = new TableCell();
+                        firstname.Text = users[i].firstname;
+                        tRow.Cells.Add(firstname);
+
+                        TableCell middlename = new TableCell();
+                        middlename.Text = users[i].middlename;
+                        tRow.Cells.Add(middlename);
+
+                        TableCell lastname = new TableCell();
+                        lastname.Text = users[i].lastname;
+                        tRow.Cells.Add(lastname);
+
+                        TableCell email = new TableCell();
+                        email.Text = users[i].email;
+                        tRow.Cells.Add(email);
+
+                        TableCell cellular = new TableCell();
+                        cellular.Text = users[i].cellular;
+                        tRow.Cells.Add(cellular);
+
+                        TableCell phone = new TableCell();
+                        phone.Text = users[i].phone;
+                        tRow.Cells.Add(phone);
+
+                        TableCell role = new TableCell();
+                        role.Text = users[i].role.roleName;
+                        tRow.Cells.Add(role);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errMsgDiv.Style.Remove("display");
+                errMsg.Text = ex.Message;
+            }
         }
     }
 }
