842dda2cb7a860aec0a557eab05e03ff55791879
princebayan <prince_bayan@hotmail.com>
2015-06-10 01:00:19 +0300

    change design view clubs

--- template/template/Admin/viewClubs.aspx
+++ template/template/Admin/viewClubs.aspx
@@ -2,5 +2,39 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>this is the view Clubs page</h1>
+   <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Hunting Clubs</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="alert alert-success" style="display:none;" id="successMsgDiv" runat="server">
+                        <p><asp:Label id="successMsg" runat="server" Text="" /></p>
+                    </div>
+                    <div class="alert alert-danger" style="display:none;" id="errMsgDiv" runat="server">
+                        <p><asp:Label id="errMsg" runat="server" Text="" /></p>
+                    </div>
+                    <div class="table-responsive">
+                        <asp:Table ID="Table1" runat="server" BorderColor="#DADDE1" CellPadding="5" CellSpacing="5" CssClass="table table-bordered table-striped">
+                            <asp:TableHeaderRow ID="TableHeaderRow1" runat="server">
+                                <asp:TableCell ID="TableCell1" runat="server"><b>Club ID</b></asp:TableCell>
+                                <asp:TableCell ID="TableCell2" runat="server"><b>Name</b></asp:TableCell>
+                                <asp:TableCell ID="TableCell3" runat="server"><b>Address</b></asp:TableCell>
+                                <asp:TableCell ID="TableCell4" runat="server"><b>Phone</b></asp:TableCell>
+                                <asp:TableCell ID="TableCell5" runat="server"><b>email</b></asp:TableCell>
+                                <asp:TableCell ID="TableCell6" runat="server"><b>Admin</b></asp:TableCell>
+                            </asp:TableHeaderRow>
+                        </asp:Table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
 </asp:Content>
--- template/template/Admin/viewClubs.aspx.cs
+++ template/template/Admin/viewClubs.aspx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using template.Controllers;
+using template.DBModel;
 
 namespace template.Admin
 {
@@ -10,7 +12,54 @@ namespace template.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                errMsgDiv.Style.Add("display", "none");
+                successMsgDiv.Style.Add("display", "none");
 
+                HuntingClubController controller = new HuntingClubController();
+
+                List<HuntingClub> clubs = controller.getHuntingClubs();
+
+                if (clubs != null)
+                {
+                    for (int i = 0; i < clubs.Count; i++)
+                    {
+                        TableRow tRow = new TableRow();
+                        Table1.Rows.Add(tRow);
+
+                        // Create a new cell and add it to the row.
+                        TableCell nameCell = new TableCell();
+                        nameCell.Text = clubs[i].clubID.ToString();
+                        tRow.Cells.Add(nameCell);
+
+                        TableCell descCell = new TableCell();
+                        descCell.Text = clubs[i].clubName;
+                        tRow.Cells.Add(descCell);
+
+                        TableCell password = new TableCell();
+                        password.Text = clubs[i].clubAddress;
+                        tRow.Cells.Add(password);
+
+                        TableCell role = new TableCell();
+                        role.Text = clubs[i].phoneNb;
+                        tRow.Cells.Add(role);
+
+                        TableCell email = new TableCell();
+                        email.Text = clubs[i].email;
+                        tRow.Cells.Add(email);
+
+                        TableCell adminID = new TableCell();
+                        adminID.Text = clubs[i].user.userID.ToString();
+                        tRow.Cells.Add(adminID);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errMsgDiv.Style.Remove("display");
+                errMsg.Text = ex.Message;
+            }
         }
     }
 }
