25cc661b67da9d9dd6386ebbe146de04097bd373
princebayan <prince_bayan@hotmail.com>
2015-06-10 00:59:57 +0300

    add hunting club controler

---
+++ template/template/Controllers/HuntingClubController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using template.DBModel;
+using template.DBService;
+
+namespace template.Controllers
+{
+    public class HuntingClubController
+    {
+        private HuntingClubService huntingClubService;
+
+        public HuntingClubController()
+        {
+            this.huntingClubService = new HuntingClubService();
+        }
+
+        public bool addHuntingClub(HuntingClub club)
+        {
+            bool result = false;
+            result = this.huntingClubService.add(club);
+            return result;
+        }
+
+        public List<HuntingClub> getHuntingClubs(int clubID= 0)
+        {
+            return this.huntingClubService.getClubs(clubID);
+        }
+    }
+}
