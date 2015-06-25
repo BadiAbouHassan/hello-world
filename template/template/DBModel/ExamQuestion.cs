c23c1730b1451a3a96143add2188f2b5912b61c3
BadiAbouHassan <badi_a_h@hotmail.com>
2015-06-25 05:41:09 +0300

    Update how the question wizard gets the question list.

--- template/template/DBModel/ExamQuestion.cs
+++ template/template/DBModel/ExamQuestion.cs
@@ -4,13 +4,13 @@ using System.Web;
 
 namespace template.DBModel
 {
-    public class ExamQuestions
+    public class ExamQuestion
     {
         public int examQuestionID { get; set; }
         public int examInstanceID { get; set; }
         public int questionID { get; set; }
 
-        public ExamQuestions()
+        public ExamQuestion()
         {
         }
 
